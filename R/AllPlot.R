ee0e064dd3c807920578869c8af629b64e7a0387
Peter Ruckdeschel <Peter.Ruckdeschel@itwm.fraunhofer.de>
2008-10-10 00:00:00 +0000

    version 2.0.1

--- R/AllPlot.R
+++ R/AllPlot.R
@@ -8,10 +8,21 @@ setMethod("plot", "ParamFamily",
     })
 setMethod("plot", "L2ParamFamily",
     function(x,y=NULL,...){
+
+        dots <- match.call(call = sys.call(sys.parent(1)), 
+                       expand.dots = FALSE)$"..."
+
+        if(!is.null(dots[["lty"]]))  dots["lty"] <- NULL
+        if(!is.null(dots[["type"]])) dots["type"] <- NULL
+        if(!is.null(dots[["main"]])) dots["main"] <- NULL
+        if(!is.null(dots[["sub"]]))  dots["sub"] <- NULL
+        if(!is.null(dots[["xlab"]])) dots["xlab"] <- NULL
+        if(!is.null(dots[["ylab"]])) dots["ylab"] <- NULL
+
         e1 <- x@distribution
         if(!is(e1, "UnivariateDistribution")) stop("not yet implemented")
 
-        plot(e1)
+        do.call(plot, c(list(e1),dots))
 
         if(is(e1, "AbscontDistribution")){
             lower <- ifelse(is.finite(q(e1)(0)), q(e1)(0), q(e1)(getdistrOption("TruncQuantile")))
@@ -32,7 +43,7 @@ setMethod("plot", "L2ParamFamily",
                 lty <- "dotted"
             }
         }
-
+        
         dims <- length(x@param)
         L2deriv <- as(diag(dims) %*% x@L2deriv, "EuclRandVariable")
 
@@ -43,21 +54,29 @@ setMethod("plot", "L2ParamFamily",
         nrows <- trunc(sqrt(dims))
         ncols <- ceiling(dims/nrows)
         par(mfrow = c(nrows, ncols))
+
+        if(is.null(dots[["cex.main"]])) dots["cex.main"] <- 0.8
+                
         for(i in 1:dims){
-            plot(x.vec, sapply(x.vec, L2deriv@Map[[i]]), type = plty, lty = lty,
-                 xlab = "x", ylab = expression(paste(L[2], " derivative")))
+            do.call(plot, args=c(list(x=x.vec, y=sapply(x.vec, L2deriv@Map[[i]]), 
+                                 type = plty, lty = lty,
+                                 xlab = "x", 
+                                 ylab = expression(paste(L[2], " derivative"))),
+                                 dots))
             if(is(e1, "DiscreteDistribution")){
                 x.vec1 <- seq(from = min(x.vec), to = max(x.vec), length = 1000)
-                lines(x.vec1, sapply(x.vec1, L2deriv@Map[[i]]), lty = "dotted")
+                do.call(lines, args=c(list(x.vec1, sapply(x.vec1, L2deriv@Map[[i]]), 
+                              lty = "dotted"),dots))
             }
             if(is.null(x@param@nuisance))
-                title(paste("Component", i, "of L_2 derivative\nof", name(x)[1], 
-                            "\nwith main parameter (", paste(round(x@param@main, 3), collapse = ", "), ")"), cex.main = 0.8)
+                do.call(title, args = c(list(paste("Component", i, "of L_2 derivative\nof", name(x)[1], 
+                            "\nwith main parameter (", paste(round(x@param@main, 3), collapse = ", "), ")")), 
+                            dots))
             else
-                title(paste("Component", i, "of L_2 derivative of", name(x)[1], 
+                do.call(title, args = c(list(paste("Component", i, "of L_2 derivative of", name(x)[1], 
                             "\nwith main parameter (", paste(round(x@param@main, 3), collapse = ", "),
-                            ")\nand nuisance parameter (", paste(round(x@param@nuisance, 3), collapse = ", "), ")"), 
-                      cex.main = 0.8)
+                            ")\nand nuisance parameter (", paste(round(x@param@nuisance, 3), collapse = ", "), ")")), 
+                            dots))
         }
         par(opar)    
         options(w0)
